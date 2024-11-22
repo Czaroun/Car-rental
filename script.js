ecf653d846b8f78ee2cc51a78c0c47bdd8ae2439
Krystian laptop <beduch_krystian@o2.pl>
2024-11-22 16:20:00 +0100

    change slider and move css

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const adjustLinks = () => {
     const isLocal = window.location.protocol === 'file:';
     const links = document.querySelectorAll('.home-link'); // Wybieramy linki do strony głównej
     const portfolioLinks = document.querySelectorAll('.portfolio-link');
+    const figureSlider = document.getElementById('slider');
 
     links.forEach(link => {
         if (isLocal) {
@@ -17,6 +18,58 @@ const adjustLinks = () => {
            link.href = "https://krystianbeduch.github.io/";
        }
     });
+
+    const images = [
+        "audi_a8",
+        "audi_q8",
+        "audi_rs7",
+        "bentley_continental_gt",
+        "bmw_m2",
+        "bmw_m3",
+        "bmw_m5",
+        "bmw_m8",
+        "chevrolet_camaro",
+        "chevrolet_corvette",
+        "ferrari_488_spider",
+        "fiat_tipo",
+        "ford_mustang_gt",
+        "jaguar_xf",
+        "lamborghini_aventador",
+        "lamborghini_urus",
+        "lexus_lc_500",
+        "maserati_levante",
+        "maybach_klasa_s",
+        "mercedes_klasa_a",
+        "mercedes_klasa_c",
+        "mercedes_klasa_e",
+        "mercedes_klasa_g",
+        "mercedes_klasa_s",
+        "nissan_leaf",
+        "rolls_royce_cullinan",
+        "rolls_royce_ghost",
+        "rolls_royce_wraith",
+        "tesla_model_x",
+        "volvo_xc90"
+    ];
+
+    // let randomImages = [];
+    for (let i = 0; i < 10; i++) {
+        const random = Math.floor(Math.random() * images.length);
+        const pathToFile = "oferta/zdjecia_aut/" + images[random] + ".jpg";
+        console.log(pathToFile);
+        images.pop();
+        const img = document.createElement("img");
+        img.src = pathToFile;
+        img.alt = images[random];
+        figureSlider.appendChild(img);
+        console.log("test");
+    }
+
+
+
+
+
+
 };
 
 document.addEventListener('DOMContentLoaded', adjustLinks);
